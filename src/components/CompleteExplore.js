81ba2173bddd92cf15d4305efdfd6f9def5080fe
saisatwik99 <saisatwik99@gmail.com>
2021-12-13 22:24:58 +0530

    Modified

--- src/components/CompleteExplore.js
+++ src/components/CompleteExplore.js
@@ -44,9 +44,9 @@ export default () => {
   const cards = [
     {
       imageSrc:
-        "https://images.unsplash.com/photo-1550699026-4114bbf4fb49?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=632&q=80",
+        "https://images.unsplash.com/photo-1560889021-ef803239616d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2lsaWNvbiUyMHZhbGxleXxlbnwwfDF8MHx8&auto=format&fit=crop&w=500&q=60",
       subtitle: "Paid",
-      title: "Loachella, NYC",
+      title: "San Francisco",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       url: "https://timerse.com"
@@ -54,9 +54,9 @@ export default () => {
 
     {
       imageSrc:
-        "https://images.unsplash.com/photo-1543423924-b9f161af87e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+        "https://images.unsplash.com/photo-1592093506632-92ef342e2591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2lsaWNvbiUyMHZhbGxleXxlbnwwfDF8MHx8&auto=format&fit=crop&w=500&q=60",
       subtitle: "Free",
-      title: "Rock In Rio, Upstate",
+      title: "Linkedin, Sunnyvale",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       url: "https://timerse.com"
@@ -64,9 +64,9 @@ export default () => {
 
     {
       imageSrc:
-        "https://images.unsplash.com/photo-1509824227185-9c5a01ceba0d?ixlib=rb-1.2.1&auto=format&fit=crop&w=658&q=80",
+        "https://images.unsplash.com/photo-1583098344557-9bb5eaa4c560?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGUlMjBvZmZpY2V8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
       subtitle: "Exclusive",
-      title: "Lollapalooza, Manhattan",
+      title: "Apple",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       url: "https://timerse.com"
@@ -77,7 +77,7 @@ export default () => {
     <Container>
       <SingleColumn>
         <HeadingInfoContainer>
-          <HeadingTitle>Popular Events</HeadingTitle>
+          <HeadingTitle>Silicon Valley</HeadingTitle>
           <HeadingDescription>
             Here are some of the most popular events in New York City curated by professionals.
           </HeadingDescription>
--- src/components/ExploreCard.js
+++ src/components/ExploreCard.js
@@ -9,6 +9,9 @@ import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons.js";
 import { ReactComponent as StarIcon } from "images/star-icon.svg";
 import { ReactComponent as SvgDecoratorBlob1 } from "images/svg-decorator-blob-5.svg";
 import { ReactComponent as SvgDecoratorBlob2 } from "images/svg-decorator-blob-7.svg";
+import HeaderTop from "components/light.js";
+import Footer from "components/Footer.js";
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 
 const HeaderRow = tw.div`flex justify-between items-center flex-col xl:flex-row`;
 const Header = tw(SectionHeading)``;
@@ -59,93 +62,93 @@ const DecoratorBlob2 = styled(SvgDecoratorBlob2)`
 `;
 
 export default ({
-  heading = "Checkout the Menu",
+  heading = "Explore the World",
   tabs = {
-    Starters: [
+    Adventure: [
       {
         imageSrc:
-          "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+          "https://images.unsplash.com/photo-1500579886065-5a7705cbf1f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YWR2ZW50dXJlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=80",
         title: "Veg Mixer",
         content: "Tomato Salad & Carrot",
         price: "$5.99",
         rating: "5.0",
         reviews: "87",
-        url: "#"
+        url: "/explore/item"
       },
       {
         imageSrc:
-          "https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+          "https://images.unsplash.com/photo-1627477284767-7d815ee1104d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cm9sbGVyJTIwY29hc3RlcnxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=80",
         title: "Macaroni",
         content: "Cheese Pizza",
         price: "$2.99",
         rating: "4.8",
         reviews: "32",
-        url: "#"
+        url: "/explore/item"
       },
       {
         imageSrc:
-          "https://images.unsplash.com/photo-1476224203421-9ac39bcb3327??ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+          "https://images.unsplash.com/photo-1466443620571-05dc7f99c6e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1vdW50YXRpbyUyMGNsaW1iaW5nfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=80",
         title: "Nelli",
         content: "Hamburger & Fries",
         price: "$7.99",
         rating: "4.9",
         reviews: "89",
-        url: "#"
+        url: "/explore/item"
       },
       {
         imageSrc:
-          "https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+          "https://images.unsplash.com/photo-1494947356691-434358cea5a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHN3aW1taW5nfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=80",
         title: "Jalapeno Poppers",
         content: "Crispy Soyabeans",
         price: "$8.99",
         rating: "4.6",
         reviews: "12",
-        url: "#"
+        url: "/explore/item"
       },
       {
         imageSrc:
-          "https://images.unsplash.com/photo-1473093226795-af9932fe5856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+          "https://images.unsplash.com/photo-1519693360201-4e53a8e0d45a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHN3aW1taW5nfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=80",
         title: "Cajun Chicken",
         content: "Roasted Chicken & Egg",
         price: "$7.99",
         rating: "4.2",
         reviews: "19",
-        url: "#"
+        url: "/explore/item"
       },
       {
         imageSrc:
-          "https://images.unsplash.com/photo-1550461716-dbf266b2a8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+          "https://images.unsplash.com/photo-1536781747177-b0c7f55e3f28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHN3aW1taW5nfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=80",
         title: "Chillie Cake",
         content: "Deepfried Chicken",
         price: "$2.99",
         rating: "5.0",
         reviews: "61",
-        url: "#"
+        url: "/explore/item"
       },
       {
         imageSrc:
-          "https://images.unsplash.com/photo-1534422298391-e4f8c172dddb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+          "https://images.unsplash.com/photo-1522402836182-92ab48342d71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aGlsbCUyMHN0YXRpb258ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
         title: "Guacamole Mex",
         content: "Mexican Chilli",
         price: "$3.99",
         rating: "4.2",
         reviews: "95",
-        url: "#"
+        url: "/explore/item"
       },
       {
         imageSrc:
-          "https://images.unsplash.com/photo-1565310022184-f23a884f29da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
+          "https://images.unsplash.com/photo-1568514328861-5465017e40fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dHJhaW58ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
         title: "Carnet Nachos",
         content: "Chilli Crispy Nachos",
         price: "$3.99",
         rating: "3.9",
         reviews: "26",
-        url: "#"
+        url: "/explore/item"
       }
     ],
-    Main: getRandomCards(),
-    Soup: getRandomCards(),
-    Desserts: getRandomCards()
+    Beach: getRandomCards(),
+    HillStation: getRandomCards(),
+    Mountions: getRandomCards()
   }
 }) => {
   /*
@@ -157,79 +160,83 @@ export default ({
   const [activeTab, setActiveTab] = useState(tabsKeys[0]);
 
   return (
-    <Container>
-      <ContentWithPaddingXl>
-        <HeaderRow>
-          <Header>{heading}</Header>
-          <TabsControl>
-            {Object.keys(tabs).map((tabName, index) => (
-              <TabControl key={index} active={activeTab === tabName} onClick={() => setActiveTab(tabName)}>
-                {tabName}
-              </TabControl>
-            ))}
-          </TabsControl>
-        </HeaderRow>
+    <AnimationRevealPage>
+      <HeaderTop/>
+      <Container>
+        <ContentWithPaddingXl>
+          <HeaderRow>
+            <Header>{heading}</Header>
+            <TabsControl>
+              {Object.keys(tabs).map((tabName, index) => (
+                <TabControl key={index} active={activeTab === tabName} onClick={() => setActiveTab(tabName)}>
+                  {tabName}
+                </TabControl>
+              ))}
+            </TabsControl>
+          </HeaderRow>
 
-        {tabsKeys.map((tabKey, index) => (
-          <TabContent
-            key={index}
-            variants={{
-              current: {
-                opacity: 1,
-                scale:1,
-                display: "flex",
-              },
-              hidden: {
-                opacity: 0,
-                scale:0.8,
-                display: "none",
-              }
-            }}
-            transition={{ duration: 0.4 }}
-            initial={activeTab === tabKey ? "current" : "hidden"}
-            animate={activeTab === tabKey ? "current" : "hidden"}
-          >
-            {tabs[tabKey].map((card, index) => (
-              <CardContainer key={index}>
-                <Card className="group" href={card.url} initial="rest" whileHover="hover" animate="rest">
-                  <CardImageContainer imageSrc={card.imageSrc}>
-                    <CardRatingContainer>
-                      <CardRating>
-                        <StarIcon />
-                        {card.rating}
-                      </CardRating>
-                      <CardReview>({card.reviews})</CardReview>
-                    </CardRatingContainer>
-                    <CardHoverOverlay
-                      variants={{
-                        hover: {
-                          opacity: 1,
-                          height: "auto"
-                        },
-                        rest: {
-                          opacity: 0,
-                          height: 0
-                        }
-                      }}
-                      transition={{ duration: 0.3 }}
-                    >
-                      <CardButton>Buy Now</CardButton>
-                    </CardHoverOverlay>
-                  </CardImageContainer>
-                  <CardText>
-                    <CardTitle>{card.title}</CardTitle>
-                    <CardContent>{card.content}</CardContent>
-                    <CardPrice>{card.price}</CardPrice>
-                  </CardText>
-                </Card>
-              </CardContainer>
-            ))}
-          </TabContent>
-        ))}
-      </ContentWithPaddingXl>
-      <DecoratorBlob1 />
-      <DecoratorBlob2 />
-    </Container>
+          {tabsKeys.map((tabKey, index) => (
+            <TabContent
+              key={index}
+              variants={{
+                current: {
+                  opacity: 1,
+                  scale:1,
+                  display: "flex",
+                },
+                hidden: {
+                  opacity: 0,
+                  scale:0.8,
+                  display: "none",
+                }
+              }}
+              transition={{ duration: 0.4 }}
+              initial={activeTab === tabKey ? "current" : "hidden"}
+              animate={activeTab === tabKey ? "current" : "hidden"}
+            >
+              {tabs[tabKey].map((card, index) => (
+                <CardContainer key={index}>
+                  <Card className="group" href={card.url} initial="rest" whileHover="hover" animate="rest">
+                    <CardImageContainer imageSrc={card.imageSrc}>
+                      <CardRatingContainer>
+                        <CardRating>
+                          <StarIcon />
+                          {card.rating}
+                        </CardRating>
+                        <CardReview>({card.reviews})</CardReview>
+                      </CardRatingContainer>
+                      <CardHoverOverlay
+                        variants={{
+                          hover: {
+                            opacity: 1,
+                            height: "auto"
+                          },
+                          rest: {
+                            opacity: 0,
+                            height: 0
+                          }
+                        }}
+                        transition={{ duration: 0.3 }}
+                      >
+                        <CardButton>Explore More</CardButton>
+                      </CardHoverOverlay>
+                    </CardImageContainer>
+                    <CardText>
+                      <CardTitle>{card.title}</CardTitle>
+                      <CardContent>{card.content}</CardContent>
+                      <CardPrice>{card.price}</CardPrice>
+                    </CardText>
+                  </Card>
+                </CardContainer>
+              ))}
+            </TabContent>
+          ))}
+        </ContentWithPaddingXl>
+        <DecoratorBlob1 />
+        <DecoratorBlob2 />
+      </Container>
+      <Footer />
+    </AnimationRevealPage>
   );
 };
 
